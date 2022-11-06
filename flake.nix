4d820d9aa0badc7a95765b2ca0a4ef5f4cda88c6
Hannes Saffrich <saffrich@informatik.uni-freiburg.de>
2022-11-06 11:07:27 +0100

    Update.

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.05";
-    reflection-lib.url = "path:../reflection-lib/";
+    reflection-lib.url = "path:/home/m0rphism/Development/projects/agda/reflection-lib/";
   };
 
-  outputs = { self, nixpkgs, ... }: 
+  outputs = { self, nixpkgs, reflection-lib, ... }: 
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -19,13 +19,13 @@
     };
     lib = nixpkgs.lib;
   in {
-    packages.x86_64-linux.defaultPackage = self.x86_64-linux.kitty;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.kitty;
 
     packages.x86_64-linux.kitty = pkgs.agdaPackages.mkDerivation {
       version = "0.1.0";
       pname = "kitty";
       src = ./.;
-      buildInputs = with pkgs.agdaPackages; [ standard-library reflection-lib ];
+      buildInputs = with pkgs.agdaPackages; [ standard-library reflection-lib.packages.x86_64-linux.reflection-lib ];
 
       libraryName = "kitty";  # has to match the .agda-lib
       everythingFile = "src/Kitty/Everything.agda";  # defaults to "Everything.agda"
